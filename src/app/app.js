387461711a2f945cd05253ae4ced200bf86b2873
Daniel Silva <dani3l.dxp@gmail.com>
2021-02-11 18:30:06 -0300

    feat: instalando e configurando o webpack e babel

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Home from "../pages";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+import "./main.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Daniel Silva!!</h1>
+      </div>
+    );
+  }
+}
