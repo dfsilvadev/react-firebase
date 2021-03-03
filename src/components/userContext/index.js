9a5140b07b60bfe4a408d45fab300273eeaa94ec
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-03 12:10:34 -0300

    feat: entendendo hook useMemo

--- src/components/userContext/index.js
+++ src/components/userContext/index.js
@@ -2,10 +2,27 @@ import React from "react";
 import { ContextComponent } from "./globalContext";
 import Produto from "./produto";
 
+const sum = () => {
+  let c;
+  for (let i = 0; i < 100000000; i++) {
+    c = i + i / 1000;
+  }
+  return c;
+};
+
 const UseContextComponent = () => {
+  const [val, setVal] = React.useState(0);
+
+  console.time();
+  const a = React.useMemo(() => sum(), []);
+  console.timeEnd();
+
   return (
     <ContextComponent>
       <h1>useContext - React Hooks</h1>
+      <button onClick={() => setVal(val + 1)}>
+        {val} - {a}
+      </button>
       <Produto />
     </ContextComponent>
   );
