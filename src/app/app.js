d959e92eda614804db346728c612ecfa6b199152
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-08 12:28:45 -0300

    feat: testando os componentes

--- src/app/app.js
+++ src/app/app.js
@@ -1,124 +1,80 @@
 import React from "react";
-import Input from "../components/input";
+import Button from "../components/button";
+import Checkbox from "../components/checkbox";
+import Input from "../components/input/input";
 import Radio from "../components/radio";
+import SelectComponent from "../components/select";
+import Textarea from "../components/textarea/input";
 import "./App.css";
 
-const fields = [
-  {
-    id: "name",
-    name: "name",
-    type: "text",
-  },
-  {
-    id: "email",
-    name: "email",
-    type: "email",
-  },
-  {
-    id: "password",
-    name: "password",
-    type: "password",
-  },
-  {
-    id: "zipCode",
-    name: "zipCode",
-    type: "text",
-  },
-  {
-    id: "street",
-    name: "street",
-    type: "text",
-  },
-  {
-    id: "number",
-    name: "number",
-    type: "text",
-  },
-  {
-    id: "district",
-    name: "district",
-    type: "text",
-  },
-  {
-    id: "city",
-    name: "city",
-    type: "text",
-  },
-  {
-    id: "state",
-    name: "state",
-    type: "text",
-  },
-];
-
 const App = () => {
-  const [form, setForm] = React.useState({
+  const technology = ["Javascript", "Reactjs", "React Native", "Nodejs"];
+
+  const [inputValues, setInputValues] = React.useState({
     name: "",
     email: "",
     password: "",
-    zipCode: "",
-    street: "",
-    number: "",
-    district: "",
-    city: "",
-    state: "",
+    city: "Select your city",
+    gender: "",
+    technology: [],
+    message: "",
   });
 
-  const [response, setResponse] = React.useState(null);
-  const [radio, setRadio] = React.useState("");
+  const [radioValue, setRadioValue] = React.useState("");
+  const [checkboxValue, setCheckboxValue] = React.useState([]);
 
-  const handleChange = ({ target }) => {
-    setForm({ ...form, [target.name]: target.value });
-  };
-
-  const handleChangeRaio = ({ target }) => {
-    setRadio(target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const headers = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(form),
-    };
-    fetch("https://ranekapi.origamid.dev/json/api/usuario", headers).then(
-      (response) => {
-        setResponse(response);
-      }
-    );
+  const handleInputChange = ({ target }) => {
+    setInputValues({ ...inputValues, [target.name]: target.value });
+    console.log(inputValues);
   };
 
   return (
-    <React.Fragment>
-      <h1>Trabalhando com inputs</h1>
-      <form onSubmit={handleSubmit} method="GET">
-        {fields.map(({ id, name, type }) => {
-          return (
-            <Input
-              key={id}
-              type={type}
-              name={name}
-              value={form[name]}
-              placeholder={`Enter with your ${name}`}
-              handleChange={handleChange}
-            />
-          );
-        })}
-        <label>
-          <Radio name="questionario" value="sim" onChange={handleChangeRaio} />
-          SIM
-        </label>
-        <label>
-          <Radio name="questionario" value="não" onChange={handleChangeRaio} />
-          NÃO
-        </label>
-        {radio}
-        <button className="btn btn-medium btn-dark">Register</button>
-      </form>
-    </React.Fragment>
+    <main className="main display-flex align-items-center justify-content-center flex-direction-column">
+      <h1>Welcome</h1>
+      <div className="content-form">
+        <form>
+          <Input
+            type="text"
+            name="name"
+            value={inputValues.name}
+            onChange={handleInputChange}
+            placeholder="Enter with your name"
+          />
+          <Input
+            type="email"
+            name="email"
+            value={inputValues.email}
+            onChange={handleInputChange}
+            placeholder="Enter with your email"
+          />
+          <Input
+            type="password"
+            name="password"
+            value={inputValues.password}
+            onChange={handleInputChange}
+            placeholder="Enter with your password"
+          />
+          <SelectComponent
+            value={inputValues.city}
+            onChange={handleInputChange}
+          />
+          <Radio
+            value={radioValue}
+            name="gender"
+            options={["sim", "não"]}
+            setValue={setRadioValue}
+          />
+          <Checkbox
+            options={technology}
+            value={checkboxValue}
+            setValue={setCheckboxValue}
+          />
+
+          <Textarea placeholder="Enter with your message" />
+          <Button>Submit</Button>
+        </form>
+      </div>
+    </main>
   );
 };
 
