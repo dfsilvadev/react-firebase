1f3fe60054981d739491cb4bd98c3f06f20689f9
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-08 12:28:29 -0300

    feat: implementando o radio component

--- src/components/radio/index.js
+++ src/components/radio/index.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Radio = ({ ...props }) => {
-  return <input type="radio" {...props} />;
+const Radio = ({ options, value, setValue, ...props }) => {
+  return (
+    <>
+      {options.map((option) => (
+        <label key={option}>
+          <input
+            type="radio"
+            value={option}
+            onChange={({ target }) => setValue(target.value)}
+            {...props}
+          />
+          <span style={{ textTransform: "capitalize", marginLeft: "5px" }}>
+            {option}
+          </span>
+        </label>
+      ))}
+    </>
+  );
 };
 
 export default Radio;
