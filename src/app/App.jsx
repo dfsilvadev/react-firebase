904b59a06fbb03155dacf44c34f9548a8d7f7c84
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-16 16:11:58 -0300

    feat: resetando a estrutura do projeto para treinar hooks

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main className="main display-flex align-items-center justify-content-center flex-direction-column">
+      <h1>Welcome</h1>
+    </main>
+  );
+};
+
+export default App;
--- src/components/input/input.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./main.css";
-
-const Input = ({ ...props }) => {
-  return <input {...props} />;
-};
-
-export default Input;
