0cf82c10995ae08391004cee5c843ed195373bf9
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-18 18:59:28 -0300

    feat: aplicando a class container

--- src/pages/Repos.jsx
+++ src/pages/Repos.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Repos = () => {
   return (
-    <div>
+    <div className="container">
       <h1>Repos</h1>
     </div>
   );
--- src/pages/Starred.jsx
+++ src/pages/Starred.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Starred = () => {
   return (
-    <div>
+    <div className="container">
       <h1>Starreds</h1>
     </div>
   );
