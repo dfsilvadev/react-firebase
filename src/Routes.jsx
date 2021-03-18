2dcd46e1db71a6ffa3868def51e8a0f1e75be4fb
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-18 16:06:29 -0300

    feat: criando a estrutura da page starred e definindo as rotas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Repos from "./pages/Repos";
+import Starred from "./pages/Starred";
 
 const MyRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="repos" element={<Repos />} />
+      <Route path="starred" element={<Starred />} />
     </Routes>
   );
 };
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Starred = () => {
+  return (
+    <div>
+      <h1>Starreds</h1>
+    </div>
+  );
+};
+
+export default Starred;
