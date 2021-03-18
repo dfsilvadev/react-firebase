37ec8c3d5fb72ddbcd67eef035717f59457bfecc
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-18 13:39:42 -0300

    feat: corrigindo import do style global.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Home from "./pages/Home";
-import "./styles/global.css";
+import "./styles/global.scss";
 
 const App = () => {
   return (
