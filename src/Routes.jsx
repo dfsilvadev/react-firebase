602b40e52959ce781c9f7fed9b9b8bbf9c4f56db
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-19 17:00:41 -0300

    feat: definindo rotas aninhadas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Repos from "./pages/Repos";
-import Starred from "./pages/Starred";
+// import Repos from "./pages/Repos";
+// import Starred from "./pages/Starred";
 
 const MyRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="repos" element={<Repos />} />
-      <Route path="starred" element={<Starred />} />
+      <Route path="/*" element={<Home />} />
+      {/* <Route path="repos" element={<Repos />} />
+      <Route path="starred" element={<Starred />} /> */}
     </Routes>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Repos from "./Repos";
+import Starred from "./Starred";
 
 const Home = () => {
   return (
     <div className="container">
       <h1>Home</h1>
+      <Link to="repos">Repos</Link>
+      <Link to="starred">Starreds</Link>
+
+      <Routes>
+        <Route path="repos" element={<Repos />} />
+        <Route path="starred" element={<Starred />} />
+      </Routes>
     </div>
   );
 };
