1aa59959edceade9d2e9fa4971a64ec3a81cb763
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-20 12:05:27 -0300

    feat: implementado page Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+import "../styles/pages/Home.scss";
+
 const Home = () => {
   return (
-    <div className="container">
-      <h1>Home</h1>
-    </div>
+    <section className="container">
+      <div className="content">
+        <div className="display-count">
+          <div className="repos-count box-count">
+            <strong>Repositories</strong>
+            <span>10</span>
+          </div>
+          <div className="starred-count box-count">
+            <strong>Starreds</strong>
+            <span>10</span>
+          </div>
+          <div className="fallowers-count box-count">
+            <strong>Fallowers</strong>
+            <span>10</span>
+          </div>
+          <div className="fallowing-count box-count">
+            <strong>Fallowing</strong>
+            <span>10</span>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
