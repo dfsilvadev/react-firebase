8cc70c5cd61e2c556748f049c5e022e0c75ecf63
Daniel Silva <dani3l.dxp@gmail.com>
2021-03-24 18:36:07 -0300

    feat: definindo os styles global

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import MyRoutes from "./Routes";
+import { GlobalStyle } from "./styles/global";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <MyRoutes />
+      <GlobalStyle />
     </>
   );
 };
--- src/styles/global.js
+++ src/styles/global.js
@@ -1,6 +1,95 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  :root {
+    --white: #fff;
+    --background: #f2f3f5;
+    --gray-line: #dcdde0;
+    --text: #666666;
+    --text-highlight: #b3b9ff;
+    --title: #2e384d;
+    --red: #e83f5b;
+    --green: #33cc95;
+    --blue: #5965e0;
+    --blue-dark: #4953b8;
+    --blue-twitter: #2aa9e0;
+    --pink: #ea4c89;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    font-smoothing: antialiased;
+    text-rendering: optimizeLegibility;
+    vertical-align: baseline;
+  }
+  html { 
+    @media (max-width:1080px) {
+      font-size: 93.75%
+    }
+    @media (max-width:1080px) {
+      font-size: 87.5%
+    }
+  }
+  body {
+    color: var(--text);
+    background-color: var(--background);
+    overflow-x: hidden;
+  }
+  body, 
+  input, 
+  textarea, 
+  button {
+    font-family: "Poppins", sans-serif;
+    font-weight: 400;
+  }
+  ol,
+  ul {
+    list-style: none;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Poppins", sans-serif;
+    font-weight: 600;
+    color: var(--title);
+  }
+  span {
+    display: inline-block;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  a,
+  p {
+    font: 400 0.875rem "Roboto Mono", sans-serif;
+  }
+  button { 
+    cursor: pointer;
+  }
 
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+  ::-webkit-scrollbar {
+    width: 7px !important;
+  }
+  ::-webkit-scrollbar-track {
+    background: #232323 !important;
+  }
+  ::-webkit-scrollbar-thumb {
+    background: #232323 !important;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    background: #28f5be !important;
+  }
 
 `;
