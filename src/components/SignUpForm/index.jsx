9cd8c363e1eb46ed3021150539a7f775c86f6f13
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-26 14:54:27 -0300

    feat: limpando dados não uitlizados

--- src/components/SignUpForm/index.jsx
+++ src/components/SignUpForm/index.jsx
@@ -1,41 +1,30 @@
-import { useState } from "react";
-
-import useChangeState from "../../hooks/useChangeState";
-
 import ButtonDefault from "../ButtonDefault";
 import InputDefault from "../InputDefault";
 
 import * as S from "./styled";
 
 const SignUpForm = () => {
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const { changeSetState } = useChangeState();
-
   return (
     <S.Form>
       <InputDefault
         type="text"
         placeholder="seu nome"
-        handleChange={(e) => changeSetState(e, setName)}
+        // handleChange={(e) => changeSetState(e, setName)}
       />
       <InputDefault
         type="text"
         placeholder="seu telefone"
-        handleChange={(e) => changeSetState(e, setPhone)}
+        // handleChange={(e) => changeSetState(e, setPhone)}
       />
       <InputDefault
         type="email"
         placeholder="seu e-mail"
-        handleChange={(e) => changeSetState(e, setEmail)}
+        // handleChange={(e) => changeSetState(e, setEmail)}
       />
       <InputDefault
         type="password"
         placeholder="sua senha"
-        handleChange={(e) => changeSetState(e, setPassword)}
+        // handleChange={(e) => changeSetState(e, setPassword)}
       />
       <ButtonDefault type="submit"> Cadastrar </ButtonDefault>
     </S.Form>
