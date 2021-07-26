0ca0952f911b0f8e5a27f4b3e6b92c96137345fd
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-26 14:56:11 -0300

    feat: implementando a tela de login

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,40 @@
+import { useState } from "react";
+
+import SignInForm from "../components/SignInForm";
+
+import * as S from "../styles/pages/home";
+import SignUpForm from "../components/SignUpForm";
+
 const SignIn = () => {
-  return <h1>Sign In</h1>;
+  const [hasAnAccount, setHasAnAccount] = useState(true);
+
+  function handleSetHasAnAccount() {
+    setHasAnAccount(!hasAnAccount);
+  }
+
+  return (
+    <S.MainContent>
+      {hasAnAccount ? (
+        <S.FormContent>
+          <S.Title>Sign In</S.Title>
+          <SignInForm />
+          <p>
+            Não tem uma conta?{" "}
+            <span onClick={handleSetHasAnAccount}>Clique aqui</span>
+          </p>
+        </S.FormContent>
+      ) : (
+        <S.FormContent>
+          <S.Title>Cadastrar</S.Title>
+          <SignUpForm />
+          <p>
+            Já tem uma conta?{" "}
+            <span onClick={handleSetHasAnAccount}>Clique aqui</span>
+          </p>
+        </S.FormContent>
+      )}
+    </S.MainContent>
+  );
 };
 
 export default SignIn;
