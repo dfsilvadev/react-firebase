4abe58688682ea4dc81783258529aefeffdc9222
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-26 19:03:52 -0300

    feat: implementando um spinner próprio

---
+++ src/components/Spinner/index.jsx
@@ -0,0 +1,12 @@
+import * as S from "./styled";
+
+const Spinner = () => {
+  return (
+    <S.Spinner>
+      <div class="first-bounce"></div>
+      <div class="last-bounce"></div>
+    </S.Spinner>
+  );
+};
+
+export default Spinner;
