7ede2685e3da8e6b4220a9ee13256a6ace67aa00
Daniel Silva <dani3l.dxp@gmail.com>
2021-07-27 08:23:27 -0300

    feat: iniciando os primeiros ajustes da page dashboard para spinner

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,9 +1,35 @@
+import { useNavigate } from "react-router-dom";
+
+import Spinner from "../components/Spinner";
+
 import useAuth from "../hooks/useAuth";
+import * as S from "../styles/pages/dashboard";
 
 const Dashboard = () => {
-  const { user } = useAuth();
+  const { loading, user, signOut } = useAuth();
+  const navigate = useNavigate();
 
-  return <h1>Dashboard {user?.email}</h1>;
+  async function handleSignOut() {
+    navigate("/");
+    await signOut();
+  }
+
+  return (
+    <>
+      {loading ? (
+        <div className="spinner-content">
+          <Spinner />
+        </div>
+      ) : (
+        <S.MainDashboard>
+          <h1>Dashboard {user?.email}</h1>
+          <button type="button" onClick={handleSignOut}>
+            Sair
+          </button>
+        </S.MainDashboard>
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
---
+++ src/styles/pages/dashboard.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const MainDashboard = styled.main`
+  border: 1px solid #000;
+`;
